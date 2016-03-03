b0117399690fccd7dd18c068075efddb00dc3727
nyanpeso <tomokosugiyama@tomokos-MacBook-Air.local>
2016-03-03 02:08:10 -0800

    jquerry1

--- quick/js/custom.js
+++ quick/js/custom.js
@@ -5,5 +5,44 @@ $(document).ready(function () {
 	});
 });
 
+// smooth scroll
+$(document).ready(function () {
+	$('.scrollto a[href*=#]:not([href=#])').click(function () {
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
 
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top - 50
+				}, 1000);
+				return false;
+			}
+		}
+	});
+});
 
+// scroll to top
+$(document).ready(function(){
+	
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 800) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	
+	//Click event to scroll to top
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+	
+});
+
+// parallax
+$(function() {
+    $.stellar();
+});
