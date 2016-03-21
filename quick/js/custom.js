2aa8047ce23d67cd4c984322135038bfe2ae3c8b
nyanpeso <tomokosugiyama@tomokos-MacBook-Air.local>
2016-03-20 23:16:51 -0700

    jqery3

--- quick/js/custom.js
+++ quick/js/custom.js
@@ -68,6 +68,8 @@ $(document).ready(function() {
     });
 });
 
+
+
 // counter up
 jQuery(document).ready(function($) {
     $('.counter').counterUp({
@@ -82,4 +84,15 @@ jQuery(document).ready(function($) {
 		type: 'image'
 	});
 });
+
+// wow animate
+var wow = new WOW(
+        {
+            boxClass: 'wow', // animated element css class (default is wow)
+            animateClass: 'animated', // animation css class (default is animated)
+            offset: 100, // distance to the element when triggering the animation (default is 0)
+            mobile: false // trigger animations on mobile devices (true is default)
+        }
+);
+wow.init();
 
